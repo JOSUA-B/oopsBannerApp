e03345e02367feb4e929d7570342a7360d0aedba
josua <josua@josuas-MacBook-Air.local>
2026-03-11 18:53:21 +0530

    UC1 - Print OOPS banner

---
+++ oopsBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPS Banner Application
+ * UC1 - Print OOPS
+ * 
+ * @author Josua
+ * @version 1.0
+ */
+
+public class oopsBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+
+}
